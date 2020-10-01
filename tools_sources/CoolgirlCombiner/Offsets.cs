0958a7c127a154fc6152065b63d8b3d926d8842e
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2020-10-01 10:31:16 +0300

    Fixed .gitignore, added offsets.cs

---
+++ tools_sources/CoolgirlCombiner/Offsets.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.clusterrr.Famicom.CoolGirl
+{
+    class Offsets
+    {
+        [JsonProperty("size")]
+        public uint Size { get; set; }
+        [JsonProperty("rom_count")]
+        public uint RomCount { get; set; }
+        [JsonProperty("games_file")]
+        public string GamesFile { get; set; }
+        [JsonProperty("games")]
+        public Game[] Games { get; set; }
+    }
+}
