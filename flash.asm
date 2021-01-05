95e6984e87000d1269f3c0fe1b25c564de99cf82
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-01-05 10:41:18 +0300

    Flash comments

--- flash.asm
+++ flash.asm
@@ -9,7 +9,7 @@ flash_detect:
   jsr enable_flash_write
   ; enter flash CFI mode
   lda #$98
-  sta $8AAA
+  sta $8AAA ; $98 -> $0AAA
   ; check for CFI signature
   lda $8020
   cmp #'Q'
@@ -34,19 +34,19 @@ sector_erase:
   jsr enable_flash_write
   jsr flash_set_superbank
   lda #$F0
-  sta $8000 ; write_prg_flash_command(0x0000, 0xF0);
+  sta $8000 ; $F0 -> $0000
   lda #$AA
-  sta $8AAA ; write_prg_flash_command(0x0AAA, 0xAA);
+  sta $8AAA ; $AA -> $0AAA
   lda #$55
-  sta $8555 ; write_prg_flash_command(0x0555, 0x55);
+  sta $8555 ; $55 -> $0555
   lda #$80
-  sta $8AAA ; write_prg_flash_command(0x0AAA, 0x80);
+  sta $8AAA ; $80 -> $0AAA
   lda #$AA
-  sta $8AAA ; write_prg_flash_command(0x0AAA, 0xAA);
+  sta $8AAA ; $AA -> $0AAA
   lda #$55
-  sta $8555 ; write_prg_flash_command(0x0555, 0x55);
+  sta $8555 ; $55 -> $0555
   lda #$30
-  sta $8000 ; write_prg_flash_command(0x0000, 0x30);
+  sta $8000 ; $30 -> $0000
   jsr disable_flash_write
 .wait:
   lda $8000
@@ -62,13 +62,13 @@ write_flash:
   ldx #$20
 .loop:
   lda #$F0
-  sta $8000 ; write_prg_flash_command(0x0000, 0xF0);
+  sta $8000 ; $F0 -> $0000
   lda #$AA
-  sta $8AAA ; write_prg_flash_command(0x0AAA, 0xAA);
+  sta $8AAA ; $AA -> $0AAA
   lda #$55
-  sta $8555 ; write_prg_flash_command(0x0555, 0x55);
+  sta $8555 ; $55 -> $0555
   lda #$A0
-  sta $8AAA ; write_prg_flash_command(0x0AAA, 0xA0);
+  sta $8AAA ; $A0 -> $0AAA
   lda [COPY_SOURCE_ADDR], y
   sta [COPY_DEST_ADDR], y
 .check1:
