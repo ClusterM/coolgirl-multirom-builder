5a28b4de7e2b33946cb424eb88a2b53538113637
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2023-05-11 13:27:38 +0400

    Typo fix

--- tools_sources/CoolgirlCombiner/Program.cs
+++ tools_sources/CoolgirlCombiner/Program.cs
@@ -35,7 +35,7 @@ namespace com.clusterrr.Famicom.CoolGirl
 #if !INTERIM
                     $"v{versionStr}"
 #else
-                    "intrerim version"
+                    "interim version"
 #endif
 #if DEBUG
                     + " (debug)"
